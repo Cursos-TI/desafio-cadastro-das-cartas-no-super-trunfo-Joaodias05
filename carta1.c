e5eddd3813eeacc3bb299c198936a28b40e4365e
Joao Dias <jovilaudias@gmail.com>
2025-02-25 18:25:03 -0300

    Update carta1.c

--- carta1.c
+++ carta1.c
@@ -9,9 +9,10 @@ char codigo [50] = "B02";
 char cidade [50] = "Brasiía";
 int turismo = 17;
 float densidade = populaçao / area;
-
+float capital = pib / populaçao;
 
 printf("Estado %s\n Codigo: %s\n", estado, codigo);
 printf("Nome da cidade %s\nNumero de habitantes %f\n", cidade, populaçao);
 printf("Área da cidade %.2f KM\n Pib %f Bilhões\n", area, pib);
 printf("Quantidade de pontos Turísticos %d\n", turismo);
+printf ("Densidade populacional: %2.f Hab/KM\nPIB por capital: %.2f Milhões\n", densidade, populaçao);
